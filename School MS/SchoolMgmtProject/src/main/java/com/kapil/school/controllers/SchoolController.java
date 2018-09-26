c76adcf3252a4984fb8aa61094b920bd4dc4e80b
Kapil Arora <kapil.arora@lakshyanet.com>
2018-09-26 11:34:50 +0530

    Project Commit on 26-09

--- School MS/SchoolMgmtProject/src/main/java/com/kapil/school/controllers/SchoolController.java
+++ School MS/SchoolMgmtProject/src/main/java/com/kapil/school/controllers/SchoolController.java
@@ -1,13 +1,19 @@
 package com.kapil.school.controllers;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-@RestController
+import com.kapil.school.dto.StudentDTO;
+
+@Controller
 public class SchoolController implements Serializable {
 
 	/**
@@ -18,10 +24,21 @@ public class SchoolController implements Serializable {
 	@Value("${spring.application.name}")
 	String appName;
 
+	@Value("${spring.student.api}")
+	String studentAPI;
+
+	@Value("${spring.application.schoolName}")
+	String schoolName;
+
 	@RequestMapping("/")
 	public String home(Model model) {
-		System.out.println(appName);
 		model.addAttribute("message", appName);
+		model.addAttribute("schoolName", schoolName);
+
+		RestTemplate restTemplate = new RestTemplate();
+		StudentDTO[] students = restTemplate.getForObject(studentAPI + "/all", StudentDTO[].class);
+		List<StudentDTO> studentList = Arrays.stream(students).collect(Collectors.toList());
+		model.addAttribute("students", studentList);	
 		return "index";
 	}
 }
